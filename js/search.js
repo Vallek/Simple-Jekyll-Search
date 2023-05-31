840f5f4025be01235147ba0009aeac30eda8c6a5
Vallek <vforval86@gmail.com>
2023-05-31 15:12:57 +0300

    added search files

---
+++ js/search.js
@@ -0,0 +1,9 @@
+SimpleJekyllSearch({
+		searchInput: document.getElementById('search-input'),
+		resultsContainer: document.getElementById('results-container'),
+		json: './search.json',
+		searchResultTemplate: '<li class="search__item"><a class="search__link" href="{url}">{title}</a></li>',
+		noResultsText: 'No results found',
+		limit: 10,
+		fuzzy: false,
+});
